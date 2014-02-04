ebbba44c0355189548c7766d46452c1e4d014841
TANGUY Arnaud <arn.tanguy@gmail.com>
2014-02-04 15:05:24 +0100

    Execute perceptron on loads of neurons... output operators

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <array>
 
 #include <iostream>
+#include <sstream>
 #include "openCLUtilities.hpp"
 
 using namespace std;
@@ -68,33 +69,14 @@ class NeuronLayer
   }
 
   void setWeights(std::initializer_list<T> init) {
-      cout << "size: " << m_size <<"," << m_out_size <<", " << m_size * m_out_size << ", init: " << init.size() << endl;
       if(m_out_layer != nullptr && init.size() != m_size * m_out_size) {
-          cout << "in: " << m_size << ", out: " << m_out_size << endl;
           throw "Your initializer list for weights exeeds the maximum size!";
       }
 
       int j=0;
       for(const auto& val : init)
       {
-          cout << j << " " << val << endl;
           weights[j++] = val;
-      }
-  }
-
-
-  void display() const {
-      cout << "\tValues: ";
-      for(int i=0; i<m_size; i++) {
-          cout  << values[i] << "\t" ;
-      }
-      cout << "\n\tWeights: ";
-      if(m_out_layer != nullptr) {
-        for(int i=0; i<m_size*m_out_size; i++) {
-            cout << weights[i] << "\t" ;
-        }
-      } else {
-          cout << "\tNo weights defined" << endl;
       }
   }
 
@@ -145,10 +127,26 @@ class NeuronLayer
       }
   }
 
+    friend ostream& operator<< (ostream &out, const NeuronLayer& layer) {
+      out << "\tValues: ";
+      for(int i=0; i<layer.m_size; i++) {
+          out  << layer.values[i] << "\t" ;
+      }
+      out << "\n\tWeights: ";
+      if(layer.m_out_layer != nullptr) {
+        for(int i=0; i<layer.m_size*layer.m_out_size; i++) {
+            out << layer.weights[i] << "\t" ;
+        }
+      } else {
+          out << "\tNo weights defined" << endl;
+      }
+      return out;
+    }
 };
 
 
-int main()
+
+int main(int argc, char **argv)
 {
 
     //get all platforms (drivers)
@@ -165,7 +163,8 @@ int main()
         cout << "Available platform: " << p.getInfo<CL_PLATFORM_NAME>() << endl;
     }
 
-    cl::Platform default_platform = all_platforms[0];
+    cl::Platform default_platform;
+    default_platform = all_platforms[0];
     cout << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << endl;
 
     //get default device of the default platform
@@ -192,21 +191,25 @@ int main()
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
+    //NeuronLayer<cl_float> out_layer(10000, nullptr);
+    //NeuronLayer<cl_float> hidden_layer(40000, &out_layer);
+    //NeuronLayer<cl_float> input_layer(2000, &hidden_layer);
+
     NeuronLayer<cl_float> out_layer(1, nullptr);
     NeuronLayer<cl_float> hidden_layer(3, &out_layer);
-    NeuronLayer<cl_float> input_layer(3, &hidden_layer);
+    NeuronLayer<cl_float> input_layer(4, &hidden_layer);
 
     out_layer.setValues({1.});
     hidden_layer.setValues({0., 0., 0.});
-    input_layer.setValues({ 1., 2., 3.});
-    //hidden_layer.setWeights({1., 2., 3.});
-    input_layer.setWeights({1., 2., 3., 4., 5., 6., 7., 8., 9.});
-    cout << "In Layer: \n";
-    input_layer.display();
-    cout << "\nHidden Layer:\n ";
-    hidden_layer.display();
-    cout << "\nOut Layer: \n";
-    out_layer.display();
+    input_layer.setValues({ 1., 2., 3., 4.});
+    input_layer.setWeights({1., 2., 3.,
+                           4., 5., 6.,
+                           7., 8., 9.,
+                           10., 11., 12.});
+    hidden_layer.setWeights({1., 2., 3.});
+    cout << "In Layer: \n" << input_layer << endl;
+    cout << "\nHidden Layer:\n " << hidden_layer << endl;
+    cout << "\nOut Layer: \n" << out_layer << endl;
 
     //create queue to which we will push commands for the device.
     cl::Kernel perceptronKernel(program, "perceptron");
@@ -222,27 +225,14 @@ int main()
     hidden_layer.run(perceptronKernel, queue);
 
     hidden_layer.enqueueReadBuffers(queue);
-    cout << "\nHidden Layer(computed): " << endl;
-    hidden_layer.display();
+    //cout << "\nHidden Layer(computed): " << endl;
+    //hidden_layer.display();
 
     out_layer.enqueueReadBuffers(queue);
-    cout << "\nOut Layer(computed): " << endl;
-    out_layer.display();
+    cout << "\nOut Layer(computed): \n" << out_layer << endl;
 
+    end = std::chrono::system_clock::now();
 
-    //run the kernel
-    //cl::KernelFunctor perceptron_kernel(cl::Kernel(program, "perceptron"), queue, cl::NullRange, cl::NDRange(out_layer_size), cl::NullRange);
-    //perceptron_kernel(buf_in_layer_size, buf_out_layer_size, buf_values, buf_weights, buf_out_values);
-
-    //read result C from the device to array C
-    //queue.enqueueReadBuffer(buf_out_values, CL_TRUE, 0, sizeof(cl_float)*out_layer_size, out_values);
-    //end = std::chrono::system_clock::now();
-
-    //cout << "Out values for layer1: ";
-    //for(const auto& i : out_values) {
-    //    cout << i << "\t";
-    //}
-    //cout << endl;
 
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
